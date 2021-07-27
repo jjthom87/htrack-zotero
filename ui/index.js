4149ba78014c6269341776e8dc8a6f4526b38469
Jared Thomas <jcjt929@web-development-teaching.com>
2021-07-26 23:38:35 -0400

    added modal shtuff

--- ui/index.js
+++ ui/index.js
@@ -1,17 +1,21 @@
+let mainData;
+let creatorsData;
+
 let retrieveData = (api, callback) => {
   fetch(`/v1/api/records/${api}`).then(function (response) { return response.json(); }).then(function (data) {
     callback({success: true, data: data});
   }).catch(function (err) {
-    reject({success: false, message: err});
+    callback({success: false, message: err});
   });
 }
 
 retrieveData("main", function(mainRes){
+  console.log(mainRes)
   if(mainRes.success){
     retrieveData("creators", function(creatorsRes){
       if(creatorsRes.success){
-        const mainData = mainRes.data;
-        const creatorsData = creatorsRes.data;
+        mainData = mainRes.data;
+        creatorsData = creatorsRes.data;
 
         let tableBody = document.querySelector("tbody");
 
@@ -20,7 +24,7 @@ retrieveData("main", function(mainRes){
           tableBodyString += "<td>" + record.key + "</td>";
           tableBodyString += "<td>" + record.DOI + "</td>";
           tableBodyString += "<td>" + record.ISSN + "</td>";
-          // tableBodyString += "<td>" + record.abstractNote + "</td>";
+          tableBodyString += "<td class='abstract-note-td'><button id='view-abstract-note' key="+record.key+">click to view</button></td>";
           tableBodyString += "<td>" + record.date + "</td>";
           tableBodyString += "<td>" + record.issue + "</td>";
           tableBodyString += "<td>" + record.itemType + "</td>";
@@ -40,4 +44,21 @@ retrieveData("main", function(mainRes){
   } else {
     console.log(mainRes.message);
   }
-})
+});
+
+document.body.addEventListener('click',function(e){
+  if(e.target && e.target.id == 'view-abstract-note'){
+    const key = e.target.getAttribute("key");
+    const abstractNote = mainData.filter((data) => data.key == key)[0].abstractNote;
+
+    document.getElementById("abstract-note-text").textContent = abstractNote;
+    document.getElementById("abstract-note-modal").style.display = "block";
+  }
+  if(e.target && e.target.id != 'view-abstract-note'){
+    document.getElementById("abstract-note-modal").style.display = "none";
+  }
+});
+
+document.getElementById("close-abstract-modal").addEventListener('click', function(e){
+  document.getElementById("abstract-note-modal").style.display = "none";
+});
